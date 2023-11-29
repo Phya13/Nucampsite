4e7eaaaadba4441d60b56921f3f87e41dbb67edc
lamnu <tuanpiang98@gmail.com>
2023-11-28 18:17:54 -0600

    #nucampsite

--- 4-React-Native.nosync/nucampsite01/screens/CampsiteInfoScreen.js
+++
@@ -1,8 +0,0 @@
-import RenderCampsite from "../features/campsites/RenderCampsite";
-
-const CampsiteInfoScreen = ({ route }) => {
-  const { campsite } = route.params;
-  return <RenderCampsite campsite={campsite} />;
-};
-
-export default CampsiteInfoScreen;
