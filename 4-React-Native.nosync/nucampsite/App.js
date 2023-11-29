69d55ad7b41f50e9e6d1b5fc5db78373dc0e01ed
lamnu <tuanpiang98@gmail.com>
2023-11-28 18:15:25 -0600

    nucampsite

--- 4-React-Native.nosync/nucampsite/App.js
+++ 4-React-Native.nosync/nucampsite/App.js
@@ -1,10 +1,20 @@
-import Main from "./screens/MainComponent";
-import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- 4-React-Native.nosync/nucampsite01/screens/DirectoryScreen.js
+++ 4-React-Native.nosync/nucampsite01/screens/DirectoryScreen.js
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
-import { CAMPSITES } from "../shared/campsites";
 
-const DirectoryScreen = ({ navigation }) => {
-  const [campsites, setCampsites] = useState(CAMPSITES);
-
+const DirectoryScreen = (props) => {
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem onPress={() => navigation.navigate("CampsiteInfo", {})}>
+      <ListItem onPress={() => props.onPress(campsite.id)}>
         <Avatar source={campsite.image} rounded />
         <ListItem.Content>
           <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -19,7 +15,7 @@ const DirectoryScreen = ({ navigation }) => {
   };
   return (
     <FlatList
-      data={campsites}
+      data={props.campsites}
       renderItem={renderDirectoryItem}
       keyExtractor={(item) => item.id.toString()}
     />
