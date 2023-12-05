f0dae2fee379db214debe9eab0d0a35866bed5c4
lamnu <tuanpiang98@gmail.com>
2023-12-04 19:44:16 -0600

    favoritesScreen

--- 4-React-Native.nosync/nucampsite/screens/ExCampsiteInfoScreen.js
+++ 4-React-Native.nosync/nucampsite/screens/ExCampsiteInfoScreen.js
@@ -1,52 +1,21 @@
 import { useState } from "react";
-import { Button, FlatList, Modal, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Button, Modal } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import RenderCampsite from "../features/campsites/RenderCampsite";
 import { toggleFavorite } from "../features/favorites/favoritesSlice";
-import { Input, Rating } from "react-native-elements";
-import { postComment } from "../features/comments/commentsSlice";
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
   const comments = useSelector((state) => state.comments);
   const favorites = useSelector((state) => state.favorites);
   const [showModal, setShowModal] = useState(false);
-  const [rating, setRating] = useState(5);
-  const [author, setAuthor] = useState("");
-  const [text, setText] = useState("");
   const dispatch = useDispatch();
-
-  const handleSubmit = () => {
-    const newComment = {
-      author,
-      rating,
-      text,
-      campsiteId: campsite.id,
-    };
-    dispatch(postComment(newComment));
-    setShowModal(!showModal);
-  };
-
-  const resetForm = () => {
-    setRating(5);
-    setAuthor("");
-    setText("");
-  };
 
   const renderCommentItem = ({ item }) => {
     return (
       <View style={styles.commentItem}>
         <Text style={{ fontSize: 14 }}>{item.text}</Text>
-        <Rating
-          startingValue={item.rating}
-          imageSize={10}
-          style={{
-            alignItems: "flex-start",
-            paddingVertical: "5%",
-          }}
-          readonly
-        />
-        {/* <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text> */}
+        <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
         <Text style={{ fontSize: 12 }}>
           {`-- ${item.author}, ${item.date}`}
         </Text>
@@ -85,50 +54,9 @@ const CampsiteInfoScreen = ({ route }) => {
         onRequestClose={() => setShowModal(!showModal)}
       >
         <View style={styles.modal}>
-          <Rating
-            showRating
-            startingValue={5}
-            imageSize={40}
-            onFinishRating={(rating) => setRating(rating)}
-            style={{ paddingVertical: 10 }}
-          />
-          <Input
-            placeholder="Author"
-            leftIcon={{
-              type: "font-awesome",
-              name: "user-o",
-            }}
-            leftIconContainerStyle={{ paddingRight: 10 }}
-            onChangeText={(rating) => setRating(rating)}
-            value={author}
-          />
-          <Input
-            placeholder="Comment"
-            leftIcon={{
-              type: "font-awesome",
-              name: "comment-o",
-            }}
-            leftIconContainerStyle={{ paddingRight: 10 }}
-            onChangeText={(rating) => setRating(rating)}
-            value={text}
-          />
-
           <View style={{ margin: 10 }}>
             <Button
-              title="Submit"
-              color="#5637DD"
-              onPress={() => {
-                handleSubmit();
-                resetForm();
-              }}
-            />
-          </View>
-          <View style={{ margin: 10 }}>
-            <Button
-              onPress={() => {
-                setShowModal(!showModal);
-                resetForm();
-              }}
+              onPress={setShowModal(!showModal)}
               color="#808080"
               title="Cancel"
             />
